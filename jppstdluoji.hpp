bdc9c9a8fa02b96207e9296bb426645886e4382f
L1ttelY <42773826+L1ttelY@users.noreply.github.com>
2019-10-05 14:50:15 +0800

    Add files via upload

---
+++ jppstdluoji.hpp
@@ -0,0 +1,24 @@
+//#pragma once
+
+#define meiju(x,y) for(int x=0;i<y;x++)
+#define duiyu for
+#define dang while
+#define ranhou {
+#define zheyang }
+#define duiyu for
+#define charu insert
+#define tanchu pop
+#define xinjian new
+#define shanchu delete
+#define yichu remove
+#define mian main
+#define shuchu printf
+#define dang while
+#define ruguo if
+#define fouze else
+#define mian main
+
+typedef int zhengxing;
+typedef long long dazhengshu;
+typedef float fudianshu;
+typedef double shuangjingdufudianshu;
