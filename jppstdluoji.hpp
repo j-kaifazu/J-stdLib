2aae10254e233d429e4adad5483e61201d1542f6
天鹏2333 <39307070+DDdongdoing@users.noreply.github.com>
2019-10-05 16:27:51 +0800

    Update jppstdluoji.hpp

--- jppstdluoji.hpp
+++ jppstdluoji.hpp
@@ -1,23 +1,42 @@
-//#pragma once
+//#pragma oncefy
 
-#define meiju(p,x,y) for(int p=x;p<=y;p++)
-#define dang while
+//符号
 #define ranhou {
 #define zheyang }
+//数学运算
+#define jia +
+#define jian -
+#define cheng *
+#define chuyi /
+#define mo %
+#define kaifang(x); sqrt(x);
+//逻辑运算
+#define qie &&
+#define huo ||
+
+//语句
+//循环
+#define meiju(p,x,y) for(int p=x;p<=y;p++)
+#define dang(x)shi while(x)
 #define duiyu for
+//数据结构操作
 #define charu insert
 #define tanchu pop
 #define xinjian new
 #define shanchu delete
 #define yichu remove
+//逻辑
 #define mian main
-#define shuchu printf
 #define ruguo if
 #define huozhe else if
 #define fouze else
+//交互
+#define shuchu printf
+//杂项
 #define mian main
 #define gao main
 
+//数据类型
 typedef int zhengxing;
 typedef long long dazhengshu;
 typedef float fudianshu;
