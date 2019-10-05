442a9661c1fcd82342baf90ac511d987f20adaf6
Wtz_LASR <wtz_lasr@outlook.com>
2019-10-05 19:24:23 +0800

    重构项目结构，添加输入输出

---
+++ include/luoji.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+//符号
+#define ranhou {
+#define zheyang }
+
+//逻辑运算
+#define xiangtong ==
+#define qie &&
+#define huo ||
+
+//循环
+#define meiju(p,x,y) for(int p=x;p<=y;p++)
+#define dang(x) while(x)
+#define duiyu for
+//内存操作
+#define xinjian new
+#define shanchu delete
+//逻辑
+#define ruguo if
+#define huozhe else if
+#define fouze else
+//杂项
+#define mian main
+#define gao main 
+ 
+//数据类型
+typedef int zhengxing;
+typedef long long dazhengshu;
+typedef float fudianshu;
+typedef double shuangjingdufudianshu;
+
